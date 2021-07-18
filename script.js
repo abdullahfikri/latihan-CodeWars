014bb7ff4608a6399e3f3d983d69506ac696fa9f
Muhammad Fikri <MFikri.Dev@gmail.com>
2021-07-18 23:40:44 +0700

    Code wars 2nd commit

--- script.js
+++ script.js
@@ -1,15 +1,47 @@
+// Duplicate Encode ------------
+
 const duplicateEncode = word => word
       .toLowerCase()
       .split('')
-      .map((word, index, arr) => {
-        let status = 0 
-        arr.forEach(el => word === el ? status++ : status);
-        return status > 1 ? ')' : '('
-      })
+      // .map((word, _, arr) => {
+      //   let status = 0 
+      //   arr.forEach(el => word === el ? status++ : status);
+      //   return status > 1 ? ')' : '('
+      // })
+      .map((word, _, arr) =>arr.indexOf(word) === arr.lastIndexOf(word)? ')':'('
+      )
       .join('');
 
 console.log(duplicateEncode('Success'))
-// console.log(duplicateEncode('recede'))
-// console.log(duplicateEncode('din'))
 
-// console.log('( for single, ) for multiple')
+// Printer Errors
+const printerError = s =>{
+  // const rules =('abcdefghijklm').split('');
+  const er = s.toLowerCase().split('').reduce((err, curr) => curr > 'm' ? ++err : err,0
+  )
+
+  return `${er}/${s.length}`;
+}
+
+console.log(printerError('dsafa'))
+
+console.log(/[^a-m]/g);
+
+// Is this a triangle?
+
+const isTriangle = (a,b,c) => a+b > c && a+c > b && b+c > a
+
+console.log(isTriangle(2,4,5));
+
+//Growth of a Population
+const nbYear =(p0, percent, aug, p) =>{
+  let perc = percent / 100;
+  let arrPenduduk = p0;
+  let i=0;
+  while (arrPenduduk < p){
+    arrPenduduk += arrPenduduk * perc + aug;
+    i++;
+  }
+  return i;
+}
+console.log(nbYear(1500, 5, 100, 5000));
